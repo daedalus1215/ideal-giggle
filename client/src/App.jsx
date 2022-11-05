1f2026f1186f9103faa1ece98dafa6fa512843fb
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-11-05 14:21:23 -0400

    Add in and integrate Apollo Client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import './App.css'
-
+import SongList from './components/SongList';
 import { ApolloProvider } from 'react-apollo';
-import ApolloClient from 'apollo-client';
+import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({});
 
@@ -10,7 +10,8 @@ function App() {
 
   return (<ApolloProvider client={client}>
     <div>Lyrical</div>
-  </ApolloProvider>);
+    <SongList />
+  </ApolloProvider >);
 }
 
 export default App
