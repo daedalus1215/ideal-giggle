71242fff6980e0459a111fdf8e7556311d1e3cff
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-11-06 08:22:45 -0500

    Wire up React Router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,6 @@
 import React from 'react'
 import './App.css'
-import SongList from './components/SongList';
-import { ApolloClient, InMemoryCache, ApolloProvider, from, HttpLink } from '@apollo/client';
-import { onError } from '@apollo/client/link/error';
 
-const errorLink = onError(({ graphqlErrors, networkError }) => {
-  if (graphqlErrors) {
-    graphqlErrors.map(({ message, location, path }) => {
-      alert(`Graphql error ${message}`)
-    });
-  }
-});
-
-const link = from([
-  errorLink,
-  new HttpLink({ uri: 'http://localhost:4000/graphql' })
-])
-const client = new ApolloClient({
-  link: link,
-  cache: new InMemoryCache(),
-});
-
-function App() {
-
-  return (<ApolloProvider client={client}>
-    <div>Lyrical</div>
-    <SongList  />
-  </ApolloProvider >);
-}
+const App = ({ children }) => <div className="container">{children}</div>;
 
 export default App
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,38 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import SongList from './components/SongList';
+import { ApolloClient, InMemoryCache, ApolloProvider, from, HttpLink } from '@apollo/client';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
+import { onError } from '@apollo/client/link/error';
+
+
+const errorLink = onError(({ graphqlErrors, networkError }) => {
+  if (graphqlErrors) {
+    graphqlErrors.map(({ message, location, path }) => {
+      alert(`Graphql error ${message}`)
+    });
+  }
+});
+
+
+const link = from([
+  errorLink,
+  new HttpLink({ uri: 'http://localhost:4000/graphql' })
+])
+const client = new ApolloClient({
+  link: link,
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+   <ApolloProvider client={client}>
+    <Router history={hashHistory}>
+      <Route path="/" component={App}>
+        <IndexRoute component={SongList} />
+      </Route>
+    </Router>
+  </ApolloProvider >
   </React.StrictMode>
 )
