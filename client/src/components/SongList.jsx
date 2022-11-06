347795653dd88ec3e60bc5a3fa6f4effe7ac0188
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-11-06 07:59:05 -0500

    Display the songs that we get from graphql query

--- client/src/components/SongList.jsx
+++ client/src/components/SongList.jsx
@@ -5,18 +5,15 @@ const query = gql`
   query GetSongs {
     songs {
       title
+      id
     }
   } 
 `;
 const SongList = () => {
   const { loading, error, data } = useQuery(query);
-
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
-  console.log('data', data)
-  return (
-    <div>SongList</div>
-  );
+  return data?.songs?.map(song => <li key={song.id}>{song.title}</li>);
 };
 
 export default SongList;
