dfed1aa672d722d7f89ec1c412ff97a3b7f419f3
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-11-10 08:45:11 -0500

    Export the queries

--- client/src/queries/index.js
+++ client/src/queries/index.js
@@ -2,3 +2,4 @@ export { default as addSong } from './addSong';
 export { default as fetchSongs } from './fetchSongs';
 export { default as fetchSong } from './fetchSong';
 export { default as deleteSong } from './deleteSong';
+export { default as addLyric } from './addLyric';
