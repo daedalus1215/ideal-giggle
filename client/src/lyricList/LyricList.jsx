bf29914aef34ba9ae8bc4c4bf55253d015a3fc0b
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-11-10 09:01:43 -0500

    Fix the key on LyricList mapping

--- client/src/lyricList/LyricList.jsx
+++ client/src/lyricList/LyricList.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LyricList = ({ lyrics }) => {
     return <ul>
-        {lyrics.map(({ id, content }) => <li kley={id}>{content}</li>)}
+        {lyrics.map(({ id, content }) => <li key={id}>{content}</li>)}
     </ul>
 };
 
