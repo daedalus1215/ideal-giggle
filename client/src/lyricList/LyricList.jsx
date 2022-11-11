5fd5a28f8641404c076264562c61a4a606a8f7e0
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-11-11 08:37:08 -0500

    Optimistic update the likes

--- client/src/lyricList/LyricList.jsx
+++ client/src/lyricList/LyricList.jsx
@@ -7,14 +7,25 @@ const LyricList = ({ lyrics }) => {
     const [mutateFunction, { data, loading, error }] = useMutation(likeLyric);
 
     return <ul>
-        {lyrics.map(({ id, content }) =>
+        {lyrics.map(({ id, content, likes }) =>
             <li
                 className={styles.li}
                 key={id}>
                 {content}
                 <i className="fa fa-thumbs-up"
-                    onClick={() => mutateFunction({ variables: { id } })}
+                    onClick={() => mutateFunction({
+                        variables: { id },
+                        optimisticResponse: {
+                            __typename: 'Mutation',
+                            likeLyric: {
+                                id: id,
+                                __typename: 'LyricType',
+                                likes: likes + 1
+                            }
+                        }
+                    })}
                 ></i>
+                {likes}
             </li>)}
     </ul>
 };
